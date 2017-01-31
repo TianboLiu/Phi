a7c2eb1f26340c18079cf92feb31b4c57f9b0a2f
Tianbo Liu <tl190@grads-dd.internal.phy.duke.edu>
2017-01-30 21:59:16 -0500

    f

--- original/format.C
+++ original/format.C
@@ -96,6 +96,33 @@ int main(int argc, char * argv[]){
     file.close();
   }
 
+  if (opt == 4){//desy094
+    double Eg[7] = {3.25, 3.75, 4.25, 4.8, 5.35, 5.9, 6.45};
+    double W[7], q[7], Q[7], t, cth, ds[7], stat[7];
+    ifstream file("desy094.txt");
+    for (int i = 0; i < 9; i++) file.getline(tmp, 256);
+    FILE * f1004 = fopen("../datasets/1004.csv", "w");
+    fprintf(f1004, "W,q,Q,cth,t,ds,stat,syst+,syst-,obs,unit,experiment\n");
+    for (int i = 0; i < 7; i++){ 
+      W[i] = sqrt(pow(Eg[i] + Mp, 2) - Eg[i] * Eg[i]);
+      q[i] = (W[i] * W[i] - Mp * Mp) / (2.0 * W[i]);
+      Q[i] = sqrt((W[i] * W[i] - pow(Mp + Mphi, 2)) * (W[i] * W[i] - pow(Mp - Mphi, 2))) / (2.0 * W[i]);
+    }
+    while (file >> t >> t >> t >> ds[0] >> stat[0] >> stat[0] >> ds[1] >> stat[1] >> stat[1] >> ds[2] >> stat[2] >> stat[2] >> ds[3] >> stat[3] >> stat[3] >> ds[4] >> stat[4] >> stat[4] >> ds[5] >> stat[5] >> stat[5] >> ds[6] >> stat[6] >> stat[6]){
+      for (int i = 0; i < 7; i++){
+	if (ds[i] > 0){
+	  cth = (t + 2.0 * q[i] * sqrt(Mphi * Mphi + Q[i] * Q[i]) - Mphi * Mphi) / (2.0 * q[i] * Q[i]);
+	  fprintf(f1004, "%.6E,%.6E,%.6E,%.6E,%.6E,%.6E,%.6E,%.6E,%.6E,%s,%s,%s\n",
+		  W[i], q[i], Q[i], cth, t, ds[i], abs(stat[i]), 
+		  0.04*ds[i], 0.04*ds[i],
+		  "ds/dt", "mb/GeV2", "desy094");
+	}
+      }
+    }
+    fclose(f1004);
+    file.close();
+  }
+
 
   return 0;
 }
