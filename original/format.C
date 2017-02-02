608cae3ef41b31b70a7ff9ee9a47a604de548c0a
Tianbo Liu <tl190@grads-dd.internal.phy.duke.edu>
2017-02-02 18:34:46 -0500

    a

--- original/format.C
+++ original/format.C
@@ -433,5 +433,37 @@ int main(int argc, char * argv[]){
     cout << "1015.csv written" << endl;
   }
 
+  if (opt == 15){//SAPHIR
+    double E1, E2, W, q, Q, cth, t, t0, ds, dsup, dsdown, stat, syst, temp;
+    string files[4] = {"saphira.txt", "saphirb.txt", "saphirc.txt", "saphird.txt"};
+    FILE * f1016 = fopen("datasets/1016.csv", "w");
+    fprintf(f1016, "i,W,q,Q,cth,t,t0,obs,value,stat,syst+,syst-,unit\n");
+    int n = 0;
+    for (int j = 0; j < 4; j++){
+      ifstream file(files[j].data());
+      file >> tmp >> E1 >> E2;
+      file.getline(tmp, 256);
+      file.getline(tmp, 256);
+      W = sqrt(pow(0.5 * (E1 + E2) + Mp, 2) - pow(0.5 * (E1 + E2), 2));
+      q = (W * W - Mp * Mp) / (2.0 * W);
+      Q = sqrt((W * W - pow(Mp + Mphi, 2)) * (W * W - pow(Mp - Mphi, 2))) / (2.0 * W);
+      t0 = Mphi * Mphi - 2.0 * q * sqrt(Mphi * Mphi + Q * Q) + 2.0 * q * Q;
+      while (file >> t >> ds){
+	file >> temp >> dsup;
+	file >> temp >> dsdown;
+	t = t0 - t;
+	cth = (t + 2.0 * q * sqrt(Mphi * Mphi + Q * Q) - Mphi * Mphi) / (2.0 * q * Q);
+	stat = 0.5 * (dsup - dsdown);
+	syst = 0.1 * ds;
+	fprintf(f1016, "%d,%.6E,%.6E,%.6E,%.6E,%.6E,%.6E,%s,%.6E,%.6E,%.6E,%.6E,%s\n",
+		++n, W, q, Q, cth, t, t0, "ds/dt", ds, stat, syst, -syst, "mub/GeV2");
+      }
+      file.close();
+    }
+    fclose(f1016);
+    cout << "1016.csv written" << endl;
+  }
+
+
   return 0;
 }
